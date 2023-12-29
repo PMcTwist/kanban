3232732817a25e8a263f17adcae56550fdf6cb7f
P@ Maynard <patmaynard452@hotmail.com>
2023-12-29 10:08:05 -0500

    -Updated App to just call the root board component
    PM

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
+import Board from './components/board';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Link
-        </a>
-      </header>
-    </div>
+    <Board />
   );
 }
 
